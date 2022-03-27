7b29ca3fe9dfd9b8f7409224f66b8a7d97aba7f9
bolgheroni-ic <roberto.bolgheroni@intuitivecare.com>
2022-03-27 12:49:46 -0300

    Set attacker and defender strategies

--- src/attacker.c
+++ src/attacker.c
@@ -5,25 +5,57 @@
 #include "direction.h"
 #include "position.h"
 #include "spy.h"
+#include <stdlib.h>
 
 // Main header
 #include "attacker.h"
 
-// Macros
-#define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+position_t last_position = { -1, -1 };
+direction_t last_direction = DIR_LEFT;
 
-/*----------------------------------------------------------------------------*/
-/*                              PUBLIC FUNCTIONS                              */
-/*----------------------------------------------------------------------------*/
+// Functions
 
-direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
-
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+// avoid repeating the same movement if an obstacle is found
+int test_direction(position_t attacker_position, direction_t direction){
+    if (equal_positions(last_position, attacker_position) && last_direction.i == direction.i && last_direction.j == direction.j){
+        return 0;
+    }
+    return 1;
 }
+
+/**
+ * Main algorithm to move Attacker player in a Game.
+ * Given the player position, it should decide the next direction
+ * they will take in the field.
+ */
+direction_t execute_attacker_strategy(position_t attacker_position,
+                                      Spy defender_spy){
+    direction_t prefered_directions[] = {DIR_UP_RIGHT, DIR_RIGHT, DIR_DOWN_RIGHT};
+    int tested_directions[3];
+    int tested_directions_amount = 0;
+    int random_direction_index = rand() % 3;
+    // tries to move in one of the preferred directions, randomly chosen
+    while (tested_directions_amount < 3 
+        && !test_direction(attacker_position, prefered_directions[random_direction_index])){
+        tested_directions[random_direction_index] = 1;
+        tested_directions_amount +=1;
+        random_direction_index = rand() % 3;
+    }
+    if (tested_directions_amount < 3) {
+        last_position = attacker_position;
+        last_direction = prefered_directions[random_direction_index]; 
+        return prefered_directions[random_direction_index];
+    }
+    // if unable to move in all of the preferred directions, move towards one of the "spare" directions left, randomly chosen
+    direction_t spare_directions[] = {DIR_LEFT, DIR_UP, DIR_DOWN, DIR_UP_LEFT, DIR_DOWN_LEFT};
+    random_direction_index = rand() % 5;
+    while (!test_direction(attacker_position, spare_directions[random_direction_index])){
+        random_direction_index = rand()%5;
+    }
+    last_position = attacker_position;
+    last_direction = spare_directions[random_direction_index]; 
+    return spare_directions[random_direction_index];
+}
+
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -5,25 +5,61 @@
 #include "direction.h"
 #include "position.h"
 #include "spy.h"
+#include <stdlib.h>
 
 // Main header
 #include "defender.h"
 
-// Macros
-#define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+position_t last_position = {-1, -1};
+direction_t last_direction = DIR_RIGHT;
 
-/*----------------------------------------------------------------------------*/
-/*                              PUBLIC FUNCTIONS                              */
-/*----------------------------------------------------------------------------*/
+// Functions
 
-direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+// avoid repeating the same movement if an obstacle is found
+int test_direction(position_t defender_position, direction_t direction)
+{
+  if (equal_positions(last_position, defender_position) && last_direction.i == direction.i && last_direction.j == direction.j)
+  {
+    return 0;
+  }
+  return 1;
+}
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+/**
+ * Main algorithm to move Defender player in a Game.
+ * Given the player position, it should decide the next direction
+ * they will take in the field.
+ */
+direction_t execute_defender_strategy(position_t defender_position,
+                                      Spy attacker_spy)
+{
+  direction_t prefered_directions[] = {DIR_UP, DIR_DOWN, DIR_LEFT, DIR_UP_LEFT, DIR_DOWN_LEFT};
+  int tested_directions[3];
+  int tested_directions_amount = 0;
+  int random_direction_index = rand() % 3;
+  // tries to move in one of the preferred directions, randomly chosen
+  while (tested_directions_amount < 3 && !test_direction(defender_position, prefered_directions[random_direction_index]))
+  {
+    tested_directions[random_direction_index] = 1;
+    tested_directions_amount += 1;
+    random_direction_index = rand() % 3;
+  }
+  if (tested_directions_amount < 3)
+  {
+    last_position = defender_position;
+    last_direction = prefered_directions[random_direction_index];
+    return prefered_directions[random_direction_index];
+  }
+  // if unable to move in all of the preferred directions, move towards one of the "spare" directions left, randomly chosen
+  direction_t spare_directions[] = {DIR_RIGHT, DIR_UP_RIGHT, DIR_DOWN_RIGHT};
+  random_direction_index = rand() % 5;
+  while (!test_direction(defender_position, spare_directions[random_direction_index]))
+  {
+    random_direction_index = rand() % 5;
+  }
+  last_position = defender_position;
+  last_direction = spare_directions[random_direction_index];
+  return spare_directions[random_direction_index];
 }
 
 /*----------------------------------------------------------------------------*/
